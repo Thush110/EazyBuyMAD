66ebe585c9a32e180aa7ba766aa9fa6e5e8dd925
Thushan Alahendra <nayanajithmc@gmail.com>
2019-08-05 11:58:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/eazybuymad/AdminCategoryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eazybuymad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdminCategoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_category)
+    }
+}
